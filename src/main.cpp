ed8592155633f8bbcd6e2a97103752ed8795e29e
Ethan Tucker <ectucker1@gmail.com>
2021-01-04 21:10:46 -0600

    Setup for GLFW and IMGUI dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,94 @@
-﻿#include "main.h"
-#include "demo/indirect_demo.h"
+﻿#include <iostream>
+#include <cmath>
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl2.h"
+#include "GLFW/glfw3.h"
+
+// Print any errors from IMGUI
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
 
 int main() {
-    run_indirect_demo();
-	return 0;
+    // Initialize GLFW
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    // Create a GLFW window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Ectert Raytracer", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Get monitor DPI scale
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    float xscale, yscale;
+    glfwGetMonitorContentScale(monitor, &xscale, &yscale);
+    float scale = std::fmax(xscale, yscale);
+
+    // Initialize IMGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    ImGui::StyleColorsDark();
+    ImGui::GetStyle().ScaleAllSizes(scale);
+
+    // Attach IMGUI to GLFW window
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+
+    // While the window hasn't been closed
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll input
+        glfwPollEvents();
+
+        // Start a new frame
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Create demo window
+        ImGui::ShowDemoWindow();
+
+        // Generate IMGUI
+        ImGui::Render();
+
+        // Resize viewport and clear screen
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Actually render the UI
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap frame into the GLFW window
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    // Shutdown IMGUI
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    // Shutdown GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
